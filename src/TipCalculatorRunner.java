e52f0832da2b1ae1906c6ab4724891217b3d969f
ablirysa <ysabellea@nycstudents.net>
2022-10-08 15:06:29 -0400

    finished runner; still want to add the repeat?

--- src/TipCalculatorRunner.java
+++ src/TipCalculatorRunner.java
@@ -7,6 +7,7 @@ public class TipCalculatorRunner {
         // creating variables
         int people;
         double tip;
+        double cost;
 
         // initializing the values, from the user
         System.out.println("Welcome to the Tip Calculator!");
@@ -19,14 +20,24 @@ public class TipCalculatorRunner {
         TipCalculator bill1 = new TipCalculator(people, tip);
 
         // adding to the bill
-        System.out.print("How many meals are there? ");
-        int numOfMeals = scan.nextInt();
-        double meal;
-        if (numOfMeals > 0) {
-            System.out.print("Enter a cost in dollars and cents: ");
-            meal = scan.nextDouble();
-            bill1.addMeal(meal);
-            numOfMeals -= 1;
-        }
+        System.out.print("Enter the cost of all meals: ");
+        cost = scan.nextDouble();
+        bill1.addMeal(cost);
+
+        // printing out information
+        System.out.println("-----------------------------------");
+        System.out.printf("Total Bill Before Tip: " + "%.2f", + bill1.getTotalBillBeforeTip());
+        System.out.println();
+        System.out.printf("Tip Percentage (in decimal form): " + "%.2f", + bill1.getTipPercentage());
+        System.out.println();
+        System.out.printf("Total Tip: " + "%.2f", + bill1.tipAmount());
+        System.out.println();
+        System.out.printf("Total Bill with Tip: " + "%.2f", + bill1.totalBill());
+        System.out.println();
+        System.out.printf("Per Person Cost Before Tip: " + "%.2f", + bill1.perPersonCostBeforeTip());
+        System.out.println();
+        System.out.printf("Tip Per Person: " + "%.2f", + bill1.perPersonTipAmount());
+        System.out.println();
+        System.out.printf("Total Cost Per Person: " + "%.2f", +  bill1.perPersonTotalCost());
     }
 }
